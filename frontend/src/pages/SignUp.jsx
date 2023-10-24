139a08603e550b31ac924644696e91cb408709a5
CrosscutX <crosscutcode@gmail.com>
2023-10-23 22:58:35 -0400

    Sign up page completed and responsive

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/SignUp.css";
 
 export default function SignUp() {
   return (
     <div className="sign-up">
-      <div className="sign-up-left"></div>
+      <div className="sign-up-left">
+        <div className="sign-up-text-container">
+          <h1>The List</h1>
+          <h2>Sign Up for a new account</h2>
+          <span className="no-account">
+            Don't have an account? <Link to="/signup">Sign Up</Link>
+          </span>
+          <form className="sign-up-form">
+            <div className="form-element">
+              <label htmlFor="sign-up-email">Email</label>
+              <input type="text" name="sign-up-email" id="sign-up-email" />
+            </div>
+            <div className="form-element">
+              <label htmlFor="sign-up-username">Username</label>
+              <input
+                type="text"
+                name="sign-up-username"
+                id="sign-up-username"
+              />
+            </div>
+            <div className="form-element">
+              <label htmlFor="sign-up-password">Password</label>
+              <input
+                type="text"
+                name="sign-up-password"
+                id="sign-up-password"
+              />
+            </div>
+            <input
+              type="button"
+              className="button sign-up-button"
+              value="Submit"
+            />
+          </form>
+        </div>
+      </div>
       <div className="sign-up-right"></div>
     </div>
   );
