36b1d843f181f137e875c412f6681f7dcbc976a8
CrosscutX <crosscutcode@gmail.com>
2023-10-25 01:56:59 -0400

    setup for list/friends

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
+import List from "./pages/List";
+import Friends from "./pages/Friends";
 import "./styles/App.css";
 
 export default function App() {
@@ -11,9 +13,11 @@ export default function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/friends" element={<Friends />} />
         </Routes>
       </BrowserRouter>
     </div>
