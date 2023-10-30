6b12c5568314e62d213d86cc279a6a6a3fab0c28
CrosscutX <crosscutcode@gmail.com>
2023-10-30 13:39:55 -0400

    Basic search result page

--- frontend/src/pages/SearchResult.jsx
+++ frontend/src/pages/SearchResult.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "../styles/Search.css";
 
 export default function SearchResult() {
-  return <div>SearchResult</div>;
+  return (
+    <div className="search-result">
+      <div className="search-header">
+        <h1>Search Value</h1>
+      </div>
+    </div>
+  );
 }
