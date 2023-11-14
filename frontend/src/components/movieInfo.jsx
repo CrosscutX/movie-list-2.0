2e125e7d30bdb02ad0453473842e97090a08aac0
CrosscutX <crosscutcode@gmail.com>
2023-11-14 17:00:06 -0500

    movie info sticks to page

---
+++ frontend/src/components/movieInfo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function movieInfo() {
+  return (
+    <div className="movie-info">
+      <h1>Movie Info</h1>
+    </div>
+  );
+}
--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import ListSelector from "../components/list/ListSelector";
 import ListFilter from "../components/list/ListFilter";
 import ListMovies from "../components/list/ListMovies";
+import MovieInfo from "../components/movieInfo";
+import "../styles/Components.css";
 import "../styles/List.css";
 
 export default function List() {
   const [selectedOption, setSelectedOption] = useState("none");
   return (
     <div className="list">
+      <MovieInfo />
       <div className="list-container">
         <h1>Your Lists</h1>
         <ListSelector
