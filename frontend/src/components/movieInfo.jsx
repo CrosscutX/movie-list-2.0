ad3a44344534267f010823d95de3b0e5992b1c41
CrosscutX <crosscutcode@gmail.com>
2023-11-15 16:56:31 -0500

    Movie info header alignment

--- frontend/src/components/movieInfo.jsx
+++ frontend/src/components/movieInfo.jsx
@@ -3,8 +3,12 @@ import React from "react";
 export default function movieInfo(props) {
   return (
     <div className="movie-info">
+      <span className="x-button">X</span>
       <div className="info-header">
-        <h1>Title</h1>
+        <button type="button" className="back-button">
+          &lt; Back
+        </button>
+        <span className="movie-title">Joker</span>
       </div>
     </div>
   );
