8718df800a7a796fd575c7a143ca97d29db75a53
CrosscutX <crosscutcode@gmail.com>
2023-11-27 15:41:49 -0500

    Movie posters are being displayed

--- frontend/src/components/list/ListMovies.jsx
+++ frontend/src/components/list/ListMovies.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import Movie from "./Movie";
-import JokerPic from "../../images/Joker.jpg";
 import "../../styles/List.css";
 
 export default function ListMovies(props) {
   const [movieList, setMovieList] = useState([]);
+  const [listOfMovies, setListOfMovies] = useState([]);
   //Runs whenever the selected list changes
   useEffect(() => {
     const fetchMoviesList = async () => {
@@ -17,37 +17,32 @@ export default function ListMovies(props) {
   }, [props.selectedUserList]);
 
   useEffect(() => {
-    // const fetchMoviesData = async () => {
-    //   if (movieList !== undefined) {
-    //     const promises = movieList.map(async (id) => {
-    //       const response = await fetch(`/api/movies/info/${id}`);
-    //       const movieInfo = await response.json();
-    //       console.log(movieInfo);
-    //     });
-    //   }
-    // };
-    // fetchMoviesData();
+    const fetchMoviesData = async () => {
+      if (movieList !== undefined) {
+        const movies = await Promise.all(
+          movieList.map(async (id) => {
+            const response = await fetch(`/api/movies/info/${id}`);
+            const movieInfo = await response.json();
+            return (
+              <Movie
+                key={movieInfo.imdbID}
+                title={movieInfo.title}
+                poster={movieInfo.image}
+                showInfo={props.showInfo}
+                setShowInfo={props.setShowInfo}
+              />
+            );
+          })
+        );
+        setListOfMovies(movies);
+      }
+    };
+    fetchMoviesData();
   }, [movieList]);
-
   return (
     <div className="list-movies">
       <h2>List Movies</h2>
-      <div className="list-movies-container">
-        <Movie
-          poster={JokerPic}
-          setShowInfo={props.setShowInfo}
-          showInfo={props.showInfo}
-        />
-        <Movie poster={JokerPic} />
-        <Movie poster={JokerPic} />
-        <Movie poster={JokerPic} />
-        <Movie poster={JokerPic} />
-        <Movie poster={JokerPic} />
-        <Movie poster={JokerPic} />
-        <Movie poster={JokerPic} />
-        <Movie poster={JokerPic} />
-        <Movie poster={JokerPic} />
-      </div>
+      <div className="list-movies-container">{listOfMovies}</div>
     </div>
   );
 }
