e00a5f76c6722436984375f325aa43071ab12d4c
CrosscutX <crosscutcode@gmail.com>
2023-12-23 16:44:05 -0500

    All bug in the list changer fixed

--- frontend/src/components/list/SelectMovieListItem.jsx
+++ frontend/src/components/list/SelectMovieListItem.jsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function SelectMovieListItem(props) {
   console.log(props.list);
+  const [allCheck, setAllCheck] = useState(true);
+
+  useEffect(() => {
+    if (props.list.listName === "all") {
+      setAllCheck(false);
+    }
+  }, []);
 
   return (
     <div className="list-item">
-      <label htmlFor={props.list.listName}>{props.list.listName}</label>
-      <input type="checkbox" id={props.list.listName} />
+      {allCheck && (
+        <div className="list-item-container">
+          <label htmlFor={props.list.listName}>{props.list.listName}</label>
+          <input type="checkbox" id={props.list.listName} />
+        </div>
+      )}
     </div>
   );
 }
