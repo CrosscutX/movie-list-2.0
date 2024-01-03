5d5c7ea13ec82d9edb244ccdd5475ca668d24a86
Treyce Wheeler <t.zeryon@gmail.com>
2024-01-03 12:25:11 -0500

    Temp auth disable

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -2,19 +2,20 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const requireAuth = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization) {
-    res.status(401).json({ error: "Auth token required" });
-  }
-  const token = authorization.split(" ")[1];
-  try {
-    const { _id } = jwt.verify(token, process.env.SECRET);
-    req.user = User.findOne({ _id }).select("_id");
-    next();
-  } catch (error) {
-    console.log(error);
-    res.status(401).json({ error: "Request not authorized" });
-  }
+  // const { authorization } = req.headers;
+  // if (!authorization) {
+  //   res.status(401).json({ error: "Auth token required" });
+  // }
+  // const token = authorization.split(" ")[1];
+  // try {
+  //   const { _id } = jwt.verify(token, process.env.SECRET);
+  //   req.user = User.findOne({ _id }).select("_id");
+  //   console.log(_id);
+  //   next();
+  // } catch (error) {
+  //   res.status(401).json({ error: "Request not authorized" });
+  // }
+  next();
 };
 
 module.exports = requireAuth;
