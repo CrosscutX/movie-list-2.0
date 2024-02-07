1b2218cbb6f00ff5aeac86d8a8c769ee3bba5242
Crosscut <crosscutcode@gmail.com>
2024-02-07 12:38:42 -0500

    Search friends add/error messages added.

--- frontend/src/components/friends/FriendSearchCard.jsx
+++ frontend/src/components/friends/FriendSearchCard.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { useState } from "react";
 
 export default function FriendCard(props) {
+  const [showAddMessage, setShowAddMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+
   let logInFriends = props.logInUserFriends;
   let isFriend = false;
 
@@ -22,7 +25,19 @@ export default function FriendCard(props) {
       },
     })
       .then((response) => response.json())
-      .then((data) => {})
+      .then((data) => {
+        if (data.msg === "Friend successfully added") {
+          setShowAddMessage(true);
+          setTimeout(() => {
+            setShowAddMessage(false);
+          }, 2000);
+        } else {
+          setShowErrorMessage(true);
+          setTimeout(() => {
+            setShowErrorMessage(false);
+          }, 2000);
+        }
+      })
       .catch((error) => {
         console.error("Error adding friend", error);
       });
@@ -30,11 +45,19 @@ export default function FriendCard(props) {
   return (
     <div className="add-friend-card">
       <span className="friend-card--name">{props.friendName}</span>
-      {isFriend ? null : (
-        <button onClick={handleClick} className="add-button-friends">
-          Add
-        </button>
-      )}
+      <div className="add-button-container">
+        {isFriend ? null : (
+          <button onClick={handleClick} className="add-button-friends">
+            Add
+          </button>
+        )}
+        {showAddMessage && (
+          <span className="add-text">Friend has been added</span>
+        )}
+        {showErrorMessage && (
+          <span className="error-text">Error adding friend</span>
+        )}
+      </div>
     </div>
   );
 }
